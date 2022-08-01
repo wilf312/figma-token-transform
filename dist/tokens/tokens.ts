37a1a9da5ceb73fd65a727c9ac9e5e322148505a
okada genya <g.okada.wilf+github@gmail.com>
2022-08-01 17:13:59 +0900

    chore: dist

---
+++ dist/tokens/tokens.ts
@@ -0,0 +1,5 @@
+export const fontSizesBase = 16;
+export const colorMainPrimary = "#f20000";
+export const colorMainSecondary = "#602cfc";
+export const colorSemanticRed = "#f20000";
+export const colorSemanticBlue = "#602cfc";
--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,6 +1,25 @@
 module.exports = {
+  // style dictionaryが参照するファイル
   source: ["dist/output.json"],
 
+  // ファイル生成する
+  platforms: {
+    ts: {
+      buildPath: "dist/tokens/",
+      transformGroup: "js",
+      files: [
+        {
+          format: "customFormat", // ここで自身で定義したformat の関数名を指定する
+          destination: "tokens.ts",
+        },
+      ],
+    },
+  },
+
+  /**
+   * カスタムフォーマット
+   * https://github.com/amzn/style-dictionary/blob/main/examples/advanced/variables-in-outputs/sd.config.js
+   */
   format: {
     // Adding a custom format to show how to get an alias's name.
     customFormat: function ({ dictionary, options }) {
@@ -24,25 +43,13 @@ module.exports = {
             }
           }
 
+          // type + name で名前を構築する
           return `export const ${token.type}${token.name} = ${value};`;
         })
         .join(`\n`);
 
       console.log(all);
       return all;
-    },
-  },
-
-  platforms: {
-    ts: {
-      buildPath: "dist/tokens/",
-      transformGroup: "js",
-      files: [
-        {
-          format: "customFormat",
-          destination: "tokens.ts",
-        },
-      ],
     },
   },
 };
